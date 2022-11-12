817d698fdc8c0babb1ed805dc1a790ccd4fc11ad
Omar Fandiño <omarfandinoya@gmail.com>
2022-11-12 14:06:03 -0500

    Command functions created

--- lua/jet.lua
+++ lua/jet.lua
@@ -2,10 +2,14 @@ require("jet.config")
 local command = require("jet.commands")
 local cmd = vim.api.nvim_create_user_command
 
-cmd('JetConfig', command.jetConfig , {})
-cmd('JetConfigPlugin', command.indev(), 
-  { nargs=1,
-    complete=function(_,_,_) return { "1", "2", "3"} end})
-cmd('JetPlugins', ':e ~/.config/nvim/lua/jet/JetPlugins.lua', {})
-cmd('JetRemovePlugin', 'echo "In dev"', {})
-cmd('JetUpdate', command.indev(), {})
+cmd('JetConfig', command.jet_config(), {})
+
+cmd('JetConfigPlugin', function(otps)
+  command.jet_config_plugin(unpack(otps.fargs))
+end, { nargs=1, complete=function() return command.get_plugins() end })
+
+cmd('JetPlugins', command.jet_plugins(), {})
+
+cmd('JetRemovePlugin', command.in_dev(), {})
+
+cmd('JetUpdate', command.in_dev(), {})
